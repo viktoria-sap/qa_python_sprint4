0acb07c41bf75db7c1ee17744e4ad00bca864fbb
Viktoria <vlg.vsap@gmail.com>
2023-12-17 21:12:20 +0300

    changed tests.py

--- tests.py
+++ tests.py
@@ -3,12 +3,11 @@ import pytest
 
 class TestBooksCollector:
     @pytest.mark.parametrize('book', ['О дивный новый мир', 'Убийство в «Восточном экспрессе»', 'Горе от ума'])
-    def test_add_new_book_add_three_books(self, book):
+    def test_add_new_book_add_book(self, book):
         collector = BooksCollector()
 
         collector.add_new_book(book)
-
-        assert collector.books_genre == {book: ''}
+        assert collector.get_book_genre(book) == ''
 
     def test_set_book_genre_for_three_books(self):
         collector = BooksCollector()
@@ -20,15 +19,7 @@ class TestBooksCollector:
         collector.set_book_genre('Убийство в «Восточном экспрессе»','Детективы')
         collector.set_book_genre('Горе от ума','Комедии')
 
-        assert collector.books_genre['О дивный новый мир'] == 'Фантастика' and collector.books_genre['Убийство в «Восточном экспрессе»'] == 'Детективы' and collector.books_genre['Горе от ума'] == 'Комедии'
-
-    def test_get_book_genre_for_three_books(self):
-        collector = BooksCollector()
-
-        collector.add_new_book('Горе от ума')
-        collector.set_book_genre('Горе от ума','Комедии')
-
-        assert collector.get_book_genre('Горе от ума') == 'Комедии'
+        assert collector.get_book_genre('О дивный новый мир') == 'Фантастика' and collector.get_book_genre('Убийство в «Восточном экспрессе»') == 'Детективы' and collector.get_book_genre('Горе от ума') == 'Комедии'
 
 
     def test_get_books_with_specific_genre_for_three_books(self):
@@ -57,7 +48,7 @@ class TestBooksCollector:
         assert collector.get_books_with_specific_genre('Мультфильмы') == []
 
 
-    def test_get_books_genre_for_three_books(self):
+    def test_get_books_genre_for_book(self):
         collector = BooksCollector()
 
         collector.add_new_book('О дивный новый мир')
@@ -79,53 +70,37 @@ class TestBooksCollector:
         assert collector.get_books_for_children() == ['О дивный новый мир', 'Горе от ума']
 
 
-    def test_add_book_in_favorites_for_three_books(self):
+    def test_add_book_in_favorites_for_book(self):
         collector = BooksCollector()
 
-        collector.add_new_book('О дивный новый мир')
         collector.add_new_book('Убийство в «Восточном экспрессе»')
-        collector.add_new_book('Горе от ума')
         collector.add_book_in_favorites('Убийство в «Восточном экспрессе»')
 
-        assert collector.favorites == ['Убийство в «Восточном экспрессе»']
+        assert collector.get_list_of_favorites_books() == ['Убийство в «Восточном экспрессе»']
 
 
-    def test_add_book_in_favorites_for_three_books_already_exist(self):
+    def test_add_book_in_favorites_for_book_already_exist(self):
         collector = BooksCollector()
 
-        collector.add_new_book('О дивный новый мир')
         collector.add_new_book('Убийство в «Восточном экспрессе»')
-        collector.add_new_book('Горе от ума')
 
         collector.add_book_in_favorites('Убийство в «Восточном экспрессе»')
 
         collector.add_book_in_favorites('Убийство в «Восточном экспрессе»')
 
-        assert collector.favorites == ['Убийство в «Восточном экспрессе»']
+        assert collector.get_list_of_favorites_books() == ['Убийство в «Восточном экспрессе»']
 
 
-    def test_delete_book_from_favorites_for_three_books(self):
+    def test_delete_book_from_favorites_for_book(self):
         collector = BooksCollector()
 
-        collector.add_new_book('О дивный новый мир')
         collector.add_new_book('Убийство в «Восточном экспрессе»')
-        collector.add_new_book('Горе от ума')
         collector.add_book_in_favorites('Убийство в «Восточном экспрессе»')
         collector.delete_book_from_favorites('Убийство в «Восточном экспрессе»')
 
-        assert collector.favorites == []
+        assert collector.get_list_of_favorites_books() == []
 
 
-    def test_get_list_of_favorites_books_for_three_books(self):
-        collector = BooksCollector()
-
-        collector.add_new_book('О дивный новый мир')
-        collector.add_new_book('Убийство в «Восточном экспрессе»')
-        collector.add_new_book('Горе от ума')
-        collector.add_book_in_favorites('О дивный новый мир')
-        collector.add_book_in_favorites('Убийство в «Восточном экспрессе»')
-
-        assert collector.get_list_of_favorites_books() == ['О дивный новый мир', 'Убийство в «Восточном экспрессе»']
 
 
 
